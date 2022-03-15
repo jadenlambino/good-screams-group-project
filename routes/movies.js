e4b013188c43d05f13db6b9124f4527ff9230fa4
Kristen Reid <kreiddetroit@gmail.com>
2022-03-15 16:55:58 -0400

    added route for movie id

--- routes/movies.js
+++ routes/movies.js
@@ -9,5 +9,13 @@ router.get('/', asyncHandler(async(req, res) => {
     res.render('movies', { title: 'Movies', allMovies });
 }));
 
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id, 10)
+    const movie = await db.Movie.findByPk(id)
+
+    if (movie) {
+        res.render('movie-info', { title: movie.name, movie });
+    }
+}))
 
 module.exports = router;

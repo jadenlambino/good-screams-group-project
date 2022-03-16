fd7ca9ae03b0e9cdcbdd0019cc0429883f19f075
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-03-16 15:25:38 -0700

    giving classes to btns

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,23 +1,27 @@
-const express = require('express')
+const express = require("express");
 
 const router = express.Router();
 const db = require("../db/models");
-const { csrfProtection, asyncHandler } = require('./utils');
-const path = require('path');
-const { requireAuth } = require('../auth');
+const { csrfProtection, asyncHandler } = require("./utils");
+// const path = require("path");
+// const { requireAuth } = require("../auth");
 
-
-router.get('/', asyncHandler(async (req, res) => {
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    console.log(res);
     const reviews = await db.Review.findAll({
-        where: { movieId: 1 }
-    })
-   const jsonReviews = JSON.stringify(reviews)
-    res.json(jsonReviews)
-}));
+      include: db.User,
+      where: { movieId: 1 },
+    });
+    const jsonReviews = JSON.stringify(reviews);
+    res.json(jsonReviews);
+  })
+);
 
-router.post('/', asyncHandler(async (req, res) => {
-    
-}))
-
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {})
+);
 
 module.exports = router;

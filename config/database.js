9392376ddc69ff69aa63c44827a93536fc595cbc
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-03-16 10:56:46 -0700

    database url

--- config/database.js
+++ config/database.js
@@ -12,8 +12,7 @@ module.exports = {
     seederStorage: "sequelize",
   },
   production: {
-    use_env_variable:
-      "postgres://yjbrejcsbzhwix:07245e89b2001a2afc4300b20ecbe3a606b223052bb75188788a7ae20da632ce@ec2-3-216-221-31.compute-1.amazonaws.com:5432/d2rpik8q7g1am1",
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     seederStorage: "sequelize",
   },

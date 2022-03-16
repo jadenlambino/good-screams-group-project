b165ca91b8f610e439efba756f7ec64fde75ec6b
arianna <arianna.n.robinson@gmail.com>
2022-03-16 18:58:03 -0400

    again

--- public/javascripts/reviews_dynamic.js
+++ public/javascripts/reviews_dynamic.js
@@ -1,56 +1,36 @@
 // const db = require('../../db/models');
 
 window.addEventListener("load", async (event) => {
-<<<<<<< HEAD
+    const currentMovieId = window.location.href.split("/");
+
     const addReview = document.getElementsByClassName("add_review_btn")[0];
     const reviewsDiv = document.getElementsByClassName("reviews_description")[0];
-    const reviewsData = await fetch("/reviews");
+
+    const reviewsData = await fetch(
+        `/reviews/${currentMovieId[currentMovieId.length - 1]}`
+    );
     const jsonReviewsData = JSON.parse(await reviewsData.json());
+    console.log(jsonReviewsData);
 
     for (let el of jsonReviewsData) {
         const pTag = document.createElement("p");
+        const liTag = document.createElement("li");
         const editBtn = document.createElement("button");
         const deleteBtn = document.createElement("button");
-        pTag.className = `reviews user-${el.userId}`;
+        pTag.className = `reviews-content userid-${el.userId}`;
+        pTag.setAttribute("id", `reviewId-${el.id}`);
         pTag.innerText = el.content;
+        liTag.className = `reviews-owner userid-${el.userId}`;
+        liTag.innerText = `By: ${el.User.firstName} ${el.User.lastName}`;
+        editBtn.className = `editbtn userid-${el.userId}`;
         editBtn.innerText = "Edit";
+        deleteBtn.className = `deletebtn userid-${el.userId}`;
         deleteBtn.innerText = "Delete";
+        pTag.appendChild(liTag);
         pTag.appendChild(editBtn);
         pTag.appendChild(deleteBtn);
         reviewsDiv.appendChild(pTag);
     }
-=======
-  const currentMovieId = window.location.href.split("/");
-
-  const addReview = document.getElementsByClassName("add_review_btn")[0];
-  const reviewsDiv = document.getElementsByClassName("reviews_description")[0];
-
-  const reviewsData = await fetch(
-    `/reviews/${currentMovieId[currentMovieId.length - 1]}`
-  );
-  const jsonReviewsData = JSON.parse(await reviewsData.json());
-  console.log(jsonReviewsData);
-
-  for (let el of jsonReviewsData) {
-    const pTag = document.createElement("p");
-    const liTag = document.createElement("li");
-    const editBtn = document.createElement("button");
-    const deleteBtn = document.createElement("button");
-    pTag.className = `reviews-content userid-${el.userId}`;
-    pTag.setAttribute("id", `reviewId-${el.id}`);
-    pTag.innerText = el.content;
-    liTag.className = `reviews-owner userid-${el.userId}`;
-    liTag.innerText = `By: ${el.User.firstName} ${el.User.lastName}`;
-    editBtn.className = `editbtn userid-${el.userId}`;
-    editBtn.innerText = "Edit";
-    deleteBtn.className = `deletebtn userid-${el.userId}`;
-    deleteBtn.innerText = "Delete";
-    pTag.appendChild(liTag);
-    pTag.appendChild(editBtn);
-    pTag.appendChild(deleteBtn);
-    reviewsDiv.appendChild(pTag);
-  }
->>>>>>> main
 
     addReview.addEventListener("click", async () => {
         const res = await fetch("/reviews", {
--- routes/reviews.js
+++ routes/reviews.js
@@ -7,42 +7,22 @@ const { csrfProtection, asyncHandler } = require("./utils");
 // const { requireAuth } = require("../auth");
 
 router.get(
-  "/:id(\\d+)",
-  asyncHandler(async (req, res) => {
-    const movieId = parseInt(req.params.id, 10);
-    console.log(res);
-    const reviews = await db.Review.findAll({
-<<<<<<< HEAD
-        where: { movieId: 1 }
+    "/:id(\\d+)",
+    asyncHandler(async (req, res) => {
+        const movieId = parseInt(req.params.id, 10);
+        console.log(res);
+        const reviews = await db.Review.findAll({
+            include: db.User,
+            where: { movieId: movieId },
+        });
+        const jsonReviews = JSON.stringify(reviews);
+        res.json(jsonReviews);
     })
-    const jsonReviews = JSON.stringify(reviews)
-    res.json(jsonReviews)
-}));
-
-router.post('/', asyncHandler(async (req, res) => {
-
-}))
-
-// router.patch('/:id(\\d+)', asyncHandler(async (req, res) => {
-//     const review = await db.Review.findByPk(req.params.id);
-//     review.content = req.body.content;
-//     await review.save();
-//     res.json({ message: "Review has been updated" })
-// }))
-
-=======
-      include: db.User,
-      where: { movieId: movieId },
-    });
-    const jsonReviews = JSON.stringify(reviews);
-    res.json(jsonReviews);
-  })
 );
 
 router.post(
-  "/",
-  asyncHandler(async (req, res) => {})
+    "/",
+    asyncHandler(async (req, res) => { })
 );
->>>>>>> main
 
 module.exports = router;

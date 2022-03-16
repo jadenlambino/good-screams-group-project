e1eae5e530438b4bf043af489ccc906fcdca0387
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-03-16 10:35:20 -0700

    heruku

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const {
   db: { username, password, database, host },
-} = require('./index');
+} = require("./index");
 
 module.exports = {
   development: {
@@ -8,7 +8,13 @@ module.exports = {
     password,
     database,
     host,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
+    dialect: "postgres",
+    seederStorage: "sequelize",
+  },
+  production: {
+    use_env_variable:
+      "postgres://yjbrejcsbzhwix:07245e89b2001a2afc4300b20ecbe3a606b223052bb75188788a7ae20da632ce@ec2-3-216-221-31.compute-1.amazonaws.com:5432/d2rpik8q7g1am1",
+    dialect: "postgres",
+    seederStorage: "sequelize",
   },
 };

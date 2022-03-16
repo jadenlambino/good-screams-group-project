cb9ffd02012cb6e75932121af0935789adb2fdd0
Jaden Lambino <54949187+jadenlambino@users.noreply.github.com>
2022-03-16 16:56:09 -0700

    working on /post method for reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,6 +5,8 @@ const db = require("../db/models");
 const { csrfProtection, asyncHandler } = require('./utils');
 const path = require('path');
 const { requireAuth } = require('../auth');
+const { check, validationResult } = require('express-validator');
+const { Movie } = require('..db/models')
 
 
 router.get('/', asyncHandler(async (req, res) => {
@@ -15,8 +17,47 @@ router.get('/', asyncHandler(async (req, res) => {
     res.json(jsonReviews)
 }));
 
-router.post('/', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
+const reviewValidation = [
+    check('content')
+    .exists({ checkFalsey: true })
+    .withMessage('Please fill out the texbox.')
+]
 
+router.get('/new', csrfProtection, requireAuth, async(req,res,next) => {
+    const movies = await Movie.findAll();
+    res.render('<<postform>>', {
+        title: 'New Review',
+        movies,
+        errors: [],
+        csrfToken: req.csrfToken()
+    })
+});
+
+router.post('/new', requireAuth, csrfProtection, reviewValidation, asyncHandler(async (req, res) => {
+    const movieId = parseInt(req.params.movieId, 10);
+
+    const { content } = req.body;
+
+    const review = db.Review.build({
+        content,
+        userId: res.locals.userId,
+        movieId
+    });
+
+    const validatorErrors = validationResult(req)
+
+    if (validatorErrors.isEmpty()) {
+        await review.save();
+        res.redirect(`/movies/${movieId}`)
+    } else {
+        const errors = validatorErrors.array().map((error) => error.msg);
+        res.render('<<url for review form page>>'), {
+            title: 'New Review',
+            content,
+            errors,
+            csrfToken: req.csrfToken(),
+        }
+    }
 }))
 
 

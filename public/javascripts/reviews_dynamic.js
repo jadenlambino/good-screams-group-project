8014791039f0bfff42fca71338e3e7ee0072a42f
arianna <arianna.n.robinson@gmail.com>
2022-03-17 19:23:51 -0400

    dynamic edit almost complete

--- public/javascripts/reviews_dynamic.js
+++ public/javascripts/reviews_dynamic.js
@@ -13,23 +13,50 @@ window.addEventListener("load", async (event) => {
   for (let el of reviews) {
     const pTag = document.createElement("p");
     const liTag = document.createElement("li");
-    const editBtn = document.createElement("button");
+
     const deleteBtn = document.createElement("button");
-    pTag.className = `reviews-content userid-${el.userId}`;
+
+    const editBtn = document.createElement("button");
+    const editForm = document.createElement('form');
+    const reviewTextarea = document.createElement('textarea');
+    const submitEditBtn = document.createElement('button');
+
+    pTag.className = `reviews-content userid-${el.userId} reviewId-${el.id}`;
     pTag.setAttribute("id", `reviewId-${el.id}`);
     pTag.innerText = el.content;
-    liTag.className = `reviews-owner userid-${el.userId}`;
+
+    liTag.className = `reviews-owner userid-${el.userId} reviewId-${el.id}`;
     liTag.innerText = `By: ${el.User.firstName} ${el.User.lastName}`;
-    editBtn.className = `editbtn userid-${el.userId}`;
+
+    editForm.className = `edit-form hidden edit-form-reviewId-${el.id} reviewId-${el.id}`
+    editForm.action = `/reviews/${el.id}`
+    editForm.method = `patch`
+
+    reviewTextarea.className = `reviews-edit-content edit-content-reviewId-${el.id} reviewId-${el.id}`;
+    reviewTextarea.name = 'content'
+
+    submitEditBtn.className = `submitEditBtn editBtn-reviewId-${el.id} reviewId-${el.id}`
+    submitEditBtn.innerText = `Update Review`
+
+    editBtn.className = `editbtn userid-${el.userId} reviewId-${el.id}`;
     editBtn.innerText = "Edit";
-    deleteBtn.className = `deletebtn userid-${el.userId}`;
+    editBtn.setAttribute('id', `editbtn-reviewId-${el.id}-${el.userId}`);
+
+    editForm.appendChild(reviewTextarea)
+    editForm.appendChild(submitEditBtn)
+
+    deleteBtn.className = `deletebtn userid-${el.userId} reviewId-${el.id}`;
     deleteBtn.setAttribute("id", `deletebtn-reviewId-${el.id}`);
     deleteBtn.innerText = "Delete";
+
     pTag.appendChild(liTag);
+    pTag.appendChild(editForm);
+
     if (userId === el.userId) {
       pTag.appendChild(editBtn);
       pTag.appendChild(deleteBtn);
     }
+
     reviewsDiv.appendChild(pTag);
   }
 
@@ -43,8 +70,8 @@ window.addEventListener("load", async (event) => {
 
   const reviewsDescription = document.querySelector(".reviews_description");
   reviewsDescription.addEventListener("click", async (e) => {
-    const btn = e.target.id.split("-")[0];
-    if (btn === "deletebtn") {
+    const deletebtn = e.target.id.split("-")[0];
+    if (deletebtn === "deletebtn") {
       const reviewsId = e.target.id.split("-")[2];
       const res = await fetch(`/reviews/${reviewsId}`, {
         method: "DELETE",
@@ -59,6 +86,45 @@ window.addEventListener("load", async (event) => {
         pTag.innerText = "Error";
         reviewsDiv.appendChild(pTag);
       }
+    }
+
+    const clickedBtn = e.target.className.split(' ')[0];
+    const reviewId = e.target.className.split(' ')[2].split('-')[1]
+
+    if (clickedBtn === "editbtn") {
+      const form = document.querySelector(`.edit-form-reviewId-${reviewId}`)
+
+      if (form.classList.contains('hidden')) {
+        form.classList.remove('hidden')
+      } else {
+        form.classList.add('hidden')
+      }
+    }
+
+    if (clickedBtn === 'submitEditBtn') {
+      const currentEditBtn = document.querySelector(`.editBtn-reviewId-${reviewId}`)
+      console.log(currentEditBtn)
+      currentEditBtn.addEventListener('click', async (event) => {
+        event.preventDefault();
+
+        const currentTextarea = document.querySelector(`.edit-content-reviewId-${reviewId}`)
+        const content = currentTextarea.value;
+        console.log(content)
+
+        const res = await fetch(`/reviews/${reviewId}`, {
+          method: 'PATCH',
+          body: JSON.stringify({ content: content }),
+          headers: { 'Content-Type': 'application/json' }
+        })
+
+        const final = await res.json();
+
+        if (final.message === 'Review has been updated') {
+          const review = document.getElementById('form')
+          review.innerHTML = final.review.content
+          form.classList.add('hidden')
+        }
+      })
     }
   });
 });

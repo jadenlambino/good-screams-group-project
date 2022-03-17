1d97de68ce8609f7d3bf93eec9900b0c1c50f4ab
Kristen Reid <kreiddetroit@gmail.com>
2022-03-17 13:35:02 -0400

    more CSS work on movie info page

--- public/javascripts/reviews_dynamic.js
+++ public/javascripts/reviews_dynamic.js
@@ -1,4 +1,4 @@
-// const db = require('../../db/models');
+
 
 window.addEventListener("load", async (event) => {
   const currentMovieId = window.location.href.split("/");
@@ -10,7 +10,7 @@ window.addEventListener("load", async (event) => {
     `/reviews/${currentMovieId[currentMovieId.length - 1]}`
   );
   const jsonReviewsData = JSON.parse(await reviewsData.json());
-//   console.log(jsonReviewsData);
+
 
   for (let el of jsonReviewsData) {
     const pTag = document.createElement("p");
@@ -25,6 +25,7 @@ window.addEventListener("load", async (event) => {
     editBtn.className = `editbtn userid-${el.userId}`;
     editBtn.innerText = "Edit";
     deleteBtn.className = `deletebtn userid-${el.userId}`;
+    deleteBtn.setAttribute('id', `deletebtn-reviewId-${el.id}`)
     deleteBtn.innerText = "Delete";
     pTag.appendChild(liTag);
     pTag.appendChild(editBtn);
@@ -40,26 +41,29 @@ window.addEventListener("load", async (event) => {
     });
   });
 
-const deleteBtns = document.querySelector('.deletebtn')
 
-for (let i = 0; i < deleteBtns.length; i++) {
-    const btn = deleteBtns[i];
-    btn.addEventListener('click', async(e) => {
-        // console.log(reviewId)
-        const res = await fetch(`/reviews/${currentMovieId[currentMovieId.length - 1]}`, {
-            method: 'DELETE'
-        })
 
-        const data = await res.json()
-        if (data.message === "Success") {
-            const container = document.querySelector(`.reviews_description`)
-            container.remove()
-        } else {
-            const pTag = document.createElement("p");
-            pTag.innerText = 'Error';
-            reviewsDiv.appendChild(pTag);
-        }
-    })
-}
+
+const reviewsDescription = document.querySelector('.reviews_description');
+  reviewsDescription.addEventListener('click', async (e) => {
+
+    const btn = e.target.id.split('-')[0];
+    if (btn === 'deletebtn') {
+      const reviewsId = e.target.id.split('-')[2]
+      const res = await fetch(`/reviews/${reviewsId}`, {
+        method: 'DELETE'
+      });
+
+      const data = await res.json()
+      if (data.message === "Success") {
+        const container = document.getElementById(`reviewId-${reviewsId}`);
+        container.remove();
+      } else {
+        const pTag = document.createElement("p");
+        pTag.innerText = 'Error';
+        reviewsDiv.appendChild(pTag);
+      }
+    }
+  });
 
 });
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,16 +3,15 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/models");
 const { csrfProtection, asyncHandler } = require("./utils");
-// const path = require("path");
-// const { requireAuth } = require("../auth");
+
 
 router.get(
   "/:id(\\d+)",
   asyncHandler(async (req, res) => {
       const movieId = parseInt(req.params.id, 10);
-    console.log(res.body)
 
-    // console.log(res);
+
+
     const reviews = await db.Review.findAll({
       include: db.User,
       where: { movieId: movieId },
@@ -27,16 +26,13 @@ router.post(
   asyncHandler(async (req, res) => {})
 );
 
-// router.post("/:id(\\d+)", asyncHandler(async (req, res) => {
-//     const review = await db.Review.findByPk(req.params.id);
-//     await review.destroy()
-// }))
+
 
 router.delete('/:id(\\d+)', asyncHandler(async (req, res) => {
     console.log('you have arrived at the route handler')
     const reviewId = parseInt(req.params.id, 10)
     const review = await db.Review.findByPk(reviewId, {
-        
+
     })
 
     if (review) {

4ff82fc2c3c5e51184c73018ba4bdb99176771bb
arianna <arianna.n.robinson@gmail.com>
2022-03-20 01:09:00 -0400

    bug fix

--- public/javascripts/searchbar_dynamic.js
+++ public/javascripts/searchbar_dynamic.js
@@ -1,35 +1,35 @@
-// window.addEventListener('load', async (event) => {
-//     const searchBar = document.querySelector('.bar')
-//     // console.log(searchBar)
-//     searchBar.addEventListener('input', async (e) => {
-//         let inputValue = e.target.value;
+window.addEventListener('load', async (event) => {
+    const searchBar = document.querySelector('.bar')
+    // console.log(searchBar)
+    searchBar.addEventListener('input', async (e) => {
+        let inputValue = e.target.value;
 
-//         const res = await fetch(`/search`, {
-//             method: "POST",
-//             body: JSON.stringify({ enteredName: inputValue }),
-//             headers: { 'Content-Type': 'application/json' }
-//         })
-//         const response = await res.json()
-
-
-//         if (response.message === "Success") {
-//             console.log(response.searchResult)
-//             // const dropDown = document.querySelector('#my_drop_down');
-//             // const aTag = document.createElement('a');
-//             // response.searchResult.forEach(result => {
-//             //     aTag.innerText = result.name;
-//             //     aTag.setAttribute('href', `/movies/${result.id}`)
-//             //     dropDown.appendChild(aTag);
-//             // })
-//             // dropDown.classList.add('show');
+        const res = await fetch(`/search`, {
+            method: "POST",
+            body: JSON.stringify({ enteredName: inputValue }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        const response = await res.json()
 
 
+        if (response.message === "Success") {
+            console.log(response.searchResult)
+            const dropDown = document.querySelector('#my_drop_down');
+            const aTag = document.createElement('a');
+            response.searchResult.forEach(result => {
+                aTag.innerText = result.name;
+                aTag.setAttribute('href', `/movies/${result.id}`)
+                dropDown.appendChild(aTag);
+            })
+            dropDown.classList.add('show');
 
-//         } else {
-//             console.log("no, you dummy")
-//         }
-//         // if (response.searchResult === inputValue) {
 
-//         // }
-//     })
-// })
+
+        } else {
+            console.log("no, you dummy")
+        }
+        // if (response.searchResult === inputValue) {
+
+        // }
+    })
+})

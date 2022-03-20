46efc08d45f5c4dd4607acf9059ba445844c05df
Kristen Reid <kreiddetroit@gmail.com>
2022-03-20 12:20:58 -0400

    css work

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-// view engine setup
+
 app.set("view engine", "pug");
 
 app.use(logger("dev"));
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(sessionSecret));
 app.use(express.static(path.join(__dirname, "public")));
 
-// set up session middleware
+
 const store = new SequelizeStore({ db: sequelize });
 
 app.use(
@@ -42,7 +42,7 @@ app.use(
 );
 
 
-// create Session table if it doesn't already exist
+
 store.sync();
 app.use(restoreUser);
 app.use("/", indexRouter);
@@ -95,25 +95,5 @@ app.use((err, req, res, next) => {
   });
 });
 
-
-
-
-// catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//   const err = new Error('The requested page couldn\'t be found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// error handler
-// app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
 
 module.exports = app;
